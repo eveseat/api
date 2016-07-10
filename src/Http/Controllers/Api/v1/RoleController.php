e8f188265d33d458122141ebc996e02e4b3b12d4
Leon Jacobs <leonja511@gmail.com>
2016-07-10 11:52:45 +0200

    Fix eveseat/seat#115 by adding new endpoints for role management

--- src/Http/Controllers/Api/v1/RoleController.php
+++ src/Http/Controllers/Api/v1/RoleController.php
@@ -23,10 +23,18 @@ namespace Seat\Api\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests;
+use Illuminate\Http\Request;
+use Seat\Web\Acl\Pillow;
 use Seat\Web\Models\Acl\Role;
 
+/**
+ * Class RoleController
+ * @package Seat\Api\Http\Controllers\Api\v1
+ */
 class RoleController extends Controller
 {
+
+    use Pillow;
 
     /**
      * Display a listing of the resource.
@@ -56,5 +64,71 @@ class RoleController extends Controller
             ->first();
 
         return response()->json($role);
+    }
+
+    /**
+     * Create a new Role
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function postNew(Request $request)
+    {
+
+        $name = $request->input('name');
+
+        $this->addRole($name);
+
+        return response()->json(true);
+    }
+
+    /**
+     * Delete a role
+     *
+     * @param $role_id
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function deleteRemove($role_id)
+    {
+
+        $this->removeRole($role_id);
+
+        return response()->json(true);
+    }
+
+    /**
+     * Give a user a role
+     *
+     * @param $user_id
+     * @param $role_id
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function getGrantUserRole($user_id, $role_id)
+    {
+
+        $this->giveUserRole($user_id, $role_id);
+
+        return response()->json(true);
+
+    }
+
+    /**
+     * Remove a user from a role
+     *
+     * @param $user_id
+     * @param $role_id
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function getRevokeUserRole($user_id, $role_id)
+    {
+
+        $this->removeUserFromRole($user_id, $role_id);
+
+        return response()->json(true);
+
     }
 }
