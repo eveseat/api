9062eb3a9c15bf94602d88a37688b45c37411daf
Loïc LEUILLIOT <e.elfaus@gmail.com>
2018-07-12 17:15:20 +0200

    provide complete documentation for wallet category

--- src/Http/Controllers/Api/v2/CorporationController.php
+++ src/Http/Controllers/Api/v2/CorporationController.php
@@ -604,7 +604,86 @@ class CorporationController extends ApiController
      *          type="integer",
      *          in="path"
      *      ),
-     *      @SWG\Response(response=200, description="Successful operation"),
+     *      @SWG\Response(response=200, description="Successful operation",
+     *          @SWG\Schema(
+     *              type="object",
+     *              @SWG\Property(
+     *                  type="array",
+     *                  property="data",
+     *                  @SWG\Items(ref="#/definitions/CorporationWalletJournal")
+     *              ),
+     *              @SWG\Property(
+     *                  type="object",
+     *                  property="links",
+     *                  description="Provide pagination urls for navigation",
+     *                  @SWG\Property(
+     *                      type="string",
+     *                      format="uri",
+     *                      property="first",
+     *                      description="First page"
+     *                  ),
+     *                  @SWG\Property(
+     *                      type="string",
+     *                      format="uri",
+     *                      property="last",
+     *                      description="Last page"
+     *                  ),
+     *                  @SWG\Property(
+     *                      type="string",
+     *                      format="uri",
+     *                      property="prev",
+     *                      description="Previous page"
+     *                  ),
+     *                  @SWG\Property(
+     *                      type="string",
+     *                      format="uri",
+     *                      property="next",
+     *                      description="Next page"
+     *                  )
+     *              ),
+     *              @SWG\Property(
+     *                  type="object",
+     *                  property="meta",
+     *                  description="Information related to the paginated response",
+     *                  @SWG\Property(
+     *                      type="integer",
+     *                      property="current_page",
+     *                      description="The current page"
+     *                  ),
+     *                  @SWG\Property(
+     *                      type="integer",
+     *                      property="from",
+     *                      description="The first entity number on the page"
+     *                  ),
+     *                  @SWG\Property(
+     *                      type="integer",
+     *                      property="last_page",
+     *                      description="The last page available"
+     *                  ),
+     *                  @SWG\Property(
+     *                      type="string",
+     *                      format="uri",
+     *                      property="path",
+     *                      description="The base endpoint"
+     *                  ),
+     *                  @SWG\Property(
+     *                      type="integer",
+     *                      property="per_page",
+     *                      description="The pagination step"
+     *                  ),
+     *                  @SWG\Property(
+     *                      type="integer",
+     *                      property="to",
+     *                      description="The last entity number on the page"
+     *                  ),
+     *                  @SWG\Property(
+     *                      type="integer",
+     *                      property="total",
+     *                      description="The total of available entities"
+     *                  )
+     *              )
+     *          )
+     *      ),
      *      @SWG\Response(response=400, description="Bad request"),
      *      @SWG\Response(response=401, description="Unauthorized"),
      *     )
@@ -634,7 +713,86 @@ class CorporationController extends ApiController
      *          type="integer",
      *          in="path"
      *      ),
-     *      @SWG\Response(response=200, description="Successful operation"),
+     *      @SWG\Response(response=200, description="Successful operation",
+     *          @SWG\Schema(
+     *              type="object",
+     *              @SWG\Property(
+     *                  type="array",
+     *                  property="data",
+     *                  @SWG\Items(ref="#/definitions/CorporationWalletTransaction")
+     *              ),
+     *              @SWG\Property(
+     *                  type="object",
+     *                  property="links",
+     *                  description="Provide pagination urls for navigation",
+     *                  @SWG\Property(
+     *                      type="string",
+     *                      format="uri",
+     *                      property="first",
+     *                      description="First page"
+     *                  ),
+     *                  @SWG\Property(
+     *                      type="string",
+     *                      format="uri",
+     *                      property="last",
+     *                      description="Last page"
+     *                  ),
+     *                  @SWG\Property(
+     *                      type="string",
+     *                      format="uri",
+     *                      property="prev",
+     *                      description="Previous page"
+     *                  ),
+     *                  @SWG\Property(
+     *                      type="string",
+     *                      format="uri",
+     *                      property="next",
+     *                      description="Next page"
+     *                  )
+     *              ),
+     *              @SWG\Property(
+     *                  type="object",
+     *                  property="meta",
+     *                  description="Information related to the paginated response",
+     *                  @SWG\Property(
+     *                      type="integer",
+     *                      property="current_page",
+     *                      description="The current page"
+     *                  ),
+     *                  @SWG\Property(
+     *                      type="integer",
+     *                      property="from",
+     *                      description="The first entity number on the page"
+     *                  ),
+     *                  @SWG\Property(
+     *                      type="integer",
+     *                      property="last_page",
+     *                      description="The last page available"
+     *                  ),
+     *                  @SWG\Property(
+     *                      type="string",
+     *                      format="uri",
+     *                      property="path",
+     *                      description="The base endpoint"
+     *                  ),
+     *                  @SWG\Property(
+     *                      type="integer",
+     *                      property="per_page",
+     *                      description="The pagination step"
+     *                  ),
+     *                  @SWG\Property(
+     *                      type="integer",
+     *                      property="to",
+     *                      description="The last entity number on the page"
+     *                  ),
+     *                  @SWG\Property(
+     *                      type="integer",
+     *                      property="total",
+     *                      description="The total of available entities"
+     *                  )
+     *              )
+     *          )
+     *      ),
      *      @SWG\Response(response=400, description="Bad request"),
      *      @SWG\Response(response=401, description="Unauthorized"),
      *     )
@@ -646,7 +804,9 @@ class CorporationController extends ApiController
     public function getWalletTransactions(int $corporation_id)
     {
 
-        return WalletTransactionResource::collection(CorporationWalletTransaction::where('corporation_id', $corporation_id)
+        return WalletTransactionResource::collection(
+                CorporationWalletTransaction::with('type')
+                    ->where('corporation_id', $corporation_id)
             ->paginate());
     }
 }
--- src/Http/Resources/WalletJournalResource.php
+++ src/Http/Resources/WalletJournalResource.php
@@ -23,6 +23,8 @@
 namespace Seat\Api\Http\Resources;
 
 use Illuminate\Http\Resources\Json\Resource;
+use Seat\Eveapi\Models\Wallet\CharacterWalletJournal;
+use Seat\Eveapi\Models\Wallet\CorporationWalletJournal;
 
 class WalletJournalResource extends Resource
 {
@@ -36,25 +38,14 @@ class WalletJournalResource extends Resource
     public function toArray($request)
     {
 
-        return [
+        $definition = parent::toArray($request);
 
-            'id'              => $this->id,
-            'date'            => $this->date,
-            'ref_type'        => $this->ref_type,
-            'first_party_id'  => $this->first_party_id,
-            'first_party'     => $this->first_party,
-            'second_party_id' => $this->second_party_id,
-            'second_party'    => $this->second_party,
-            'amount'          => $this->amount,
-            'balance'         => $this->balance,
-            'reason'          => $this->reason,
-            'tax_receiver_id' => $this->tax_receiver_id,
-            'tax'             => $this->tax,
-            'context_id'      => $this->context_id,
-            'context_id_type' => $this->context_id_type,
-            'description'     => $this->description,
-            'created_at'      => $this->created_at,
-            'updated_at'      => $this->updated_at,
-        ];
+        if ($this->resource instanceof CorporationWalletJournal)
+            array_forget($definition, 'corporation_id');
+
+        if ($this->resource instanceof CharacterWalletJournal)
+            array_forget($definition, 'character_id');
+
+        return $definition;
     }
 }
