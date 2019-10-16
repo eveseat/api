a89a079cf018724956b9dde4362930fe756dd95d
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2019-10-16 18:45:26 +0200

    feat(deps): upgrade tokens logs layout

--- src/resources/views/logs.blade.php
+++ src/resources/views/logs.blade.php
@@ -5,69 +5,71 @@
 
 @section('full')
 
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h3 class="panel-title">{{ trans('api::seat.access_logs') }}</h3>
+  <div class="card">
+    <div class="card-header">
+      <h3 class="card-title">{{ trans('api::seat.access_logs') }}</h3>
     </div>
-    <div class="panel-body">
+    <div class="card-body">
 
-      <table class="table table-condensed table-hover table-responsive">
-        <tbody>
-        <tr>
-          <th>{{ trans('api::seat.date') }}</th>
-          <th>{{ trans('api::seat.action') }}</th>
-          <th>{{ trans('api::seat.request_method') }}</th>
-          <th>{{ trans('api::seat.request_path') }}</th>
-          <th>{{ trans('api::seat.source_ip') }}</th>
-        </tr>
-
-        @foreach($logs as $log)
-
+      <table class="table table-condensed table-hover">
+        <thead>
           <tr>
-            <td @if($log->action == 'deny')class="danger"@endif>
-              <span data-toggle="tooltip"
-                    title="" data-original-title="{{ $log->created_at }}">
-                {{ human_diff($log->created_at) }}
-              </span>
-            </td>
-            <td @if($log->action == 'deny')class="danger"@endif>
-              @if($log->action == 'deny')
-                <i class="fa fa-warning"></i>
-              @endif
-              {{ ucfirst($log->action) }}
-            </td>
-            <td @if($log->action == 'deny')class="danger"@endif>
-              @switch(strtolower($log->method))
-                @case('post')
-                  <span class="label label-success">{{ strtoupper($log->method) }}</span>
-                  @break
-                @case('put')
-                  <span class="label label-warning">{{ strtoupper($log->method) }}</span>
-                  @break;
-                @case('get')
-                  <span class="label label-info">{{ strtoupper($log->method) }}</span>
-                  @break;
-                @case('delete')
-                  <span class="label label-danger">{{ strtoupper($log->method) }}</span>
-                  @break;
-                @case('head')
-                  <span class="label bg-purple">{{ strtoupper($log->method) }}</span>
-                  @break;
-                @case('patch')
-                  <span class="label bg-teal">{{ strtoupper($log->method) }}</span>
-                  @break;
-                @case('options')
-                  <span class="label label-primary">{{ strtoupper($log->method) }}</span>
-                  @break;
-                @default
-                  <span class="label label-default">{{ strtoupper($log->method) }}</span>
-              @endswitch
-            </td>
-            <td @if($log->action == 'deny')class="danger"@endif>{{ $log->request_path }}</td>
-            <td @if($log->action == 'deny')class="danger"@endif>{{ $log->src_ip }}</td>
+            <th>{{ trans('api::seat.date') }}</th>
+            <th>{{ trans('api::seat.action') }}</th>
+            <th>{{ trans('api::seat.request_method') }}</th>
+            <th>{{ trans('api::seat.request_path') }}</th>
+            <th>{{ trans('api::seat.source_ip') }}</th>
           </tr>
+        </thead>
+        <tbody>
 
-        @endforeach
+          @foreach($logs as $log)
+
+            <tr>
+              <td @if($log->action == 'deny')class="text-danger"@endif>
+                <span data-toggle="tooltip"
+                      title="" data-original-title="{{ $log->created_at }}">
+                  {{ human_diff($log->created_at) }}
+                </span>
+              </td>
+              <td @if($log->action == 'deny')class="text-danger"@endif>
+                @if($log->action == 'deny')
+                  <i class="fa fa-warning"></i>
+                @endif
+                {{ ucfirst($log->action) }}
+              </td>
+              <td @if($log->action == 'deny')class="text-danger"@endif>
+                @switch(strtolower($log->method))
+                  @case('post')
+                    <span class="badge badge-success">{{ strtoupper($log->method) }}</span>
+                    @break
+                  @case('put')
+                    <span class="badge badge-warning">{{ strtoupper($log->method) }}</span>
+                    @break;
+                  @case('get')
+                    <span class="badge badge-info">{{ strtoupper($log->method) }}</span>
+                    @break;
+                  @case('delete')
+                    <span class="badge badge-danger">{{ strtoupper($log->method) }}</span>
+                    @break;
+                  @case('head')
+                    <span class="badge bg-purple">{{ strtoupper($log->method) }}</span>
+                    @break;
+                  @case('patch')
+                    <span class="badge bg-teal">{{ strtoupper($log->method) }}</span>
+                    @break;
+                  @case('options')
+                    <span class="badge badge-primary">{{ strtoupper($log->method) }}</span>
+                    @break;
+                  @default
+                    <span class="badge badge-default">{{ strtoupper($log->method) }}</span>
+                @endswitch
+              </td>
+              <td @if($log->action == 'deny')class="text-danger"@endif>{{ $log->request_path }}</td>
+              <td @if($log->action == 'deny')class="text-danger"@endif>{{ $log->src_ip }}</td>
+            </tr>
+
+          @endforeach
 
         </tbody>
       </table>
@@ -75,7 +77,7 @@
     </div>
 
     @if($logs->render())
-      <div class="panel-footer">
+      <div class="card-footer">
         {!! $logs->render() !!}
       </div>
     @endif
