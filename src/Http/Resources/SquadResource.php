faac9953e5ea2433689ae0c68e8c6694cb56c947
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2020-03-23 22:51:01 +0100

    feat(api): implement squads routes

--- src/Http/Resources/SquadResource.php
+++ src/Http/Resources/SquadResource.php
@@ -28,6 +28,34 @@ use Illuminate\Http\Resources\Json\Resource;
  * Class SquadResource.
  *
  * @package Seat\Api\Http\Resources
+ *
+ * @OA\Schema(
+ *     type="object",
+ *     title="SquadResource",
+ *     schema="SquadResource",
+ *     description="Squad Resource",
+ *     @OA\Property(property="id", type="integer", description="The unique identifier"),
+ *     @OA\Property(property="type", type="string", enum={"manual", "auto", "hidden"}, description="The Squad management type"),
+ *     @OA\Property(property="name", type="string", description="The Squad name"),
+ *     @OA\Property(property="logo", type="string", format="byte", description="The Squad Logo"),
+ *     @OA\Property(property="description", type="string", description="The squad description")
+ * )
+ *
+ * @OA\Schema(
+ *     type="object",
+ *     title="Squad",
+ *     schema="Squad",
+ *     description="Detailed Squad",
+ *     allOf={
+ *       @OA\Schema(ref="#/components/schemas/SquadResource"),
+ *       @OA\Schema(
+ *          @OA\Property(property="roles", type="array", @OA\Items(type="integer"), description="List of roles attached to that Squad"),
+ *          @OA\Property(property="moderators", type="array", @OA\Items(type="integer"), description="List of moderators attached to that Squad"),
+ *          @OA\Property(property="members", type="array", @OA\Items(type="integer"), description="List of members attached to that Squad"),
+ *          @OA\Property(property="applications", type="array", @OA\Items(type="integer"), description="List of candidates attached to that Squad")
+ *       )
+ *     }
+ * )
  */
 class SquadResource extends Resource
 {
@@ -39,7 +67,14 @@ class SquadResource extends Resource
     {
         return [
             'id' => $this->id,
+            'type' => $this->type,
             'name' => $this->name,
+            'logo' => $this->logo,
+            'description' => $this->description,
+            'roles' => $this->whenLoaded('roles', function () { return $this->roles->pluck('id'); }),
+            'moderators' => $this->whenLoaded('moderators', function () { return $this->moderators->pluck('id'); }),
+            'members' => $this->whenLoaded('members', function () { return $this->members->pluck('id'); }),
+            'applications' => $this->whenLoaded('applications', function () { return $this->applications->pluck('user_id'); }),
         ];
     }
 }
