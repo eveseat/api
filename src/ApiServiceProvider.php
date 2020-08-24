6a1b2b252a946de2f3e3101db28c949655846e44
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2020-08-24 18:33:42 +0200

    fix(api): update server definition and related url

--- src/ApiServiceProvider.php
+++ src/ApiServiceProvider.php
@@ -77,7 +77,7 @@ class ApiServiceProvider extends AbstractSeatPlugin
         config(['l5-swagger.swagger_version' => '3.0']);
 
         // Use base host configured in the .env file for the swagger host.
-        config(['l5-swagger.constants.L5_SWAGGER_CONST_HOST' => sprintf('%s/api', env('APP_URL'))]);
+        config(['l5-swagger.constants.L5_SWAGGER_CONST_HOST' => sprintf('%s/api', config('app.url'))]);
 
         // SwaggerUI long description.
         config([
