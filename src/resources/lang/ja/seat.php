9c9b3d3346cef4784d23fc97fe4fccaa08ec177d
Leon Jacobs <leonja511@gmail.com>
2022-11-23 20:50:58 +0200

    New translations seat.php (Japanese)

---
+++ src/resources/lang/ja/seat.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of SeAT
+ *
+ * Copyright (C) 2015 to 2022 Leon Jacobs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+return [
+
+    'api_token_admin'    => 'API Token Admin',
+    'new_token'          => 'New Token',
+    'key_comment'        => 'Key Comment',
+    'allowed_ip_address' => 'Allowed IP Address',
+    'ip_help'            => 'This is the source IP address that will be allowed to use the generated token.',
+    'ip_danger'          => 'Using 0.0.0.0 will allow ANYONE who has this token to use the SeAT API. This is dangerous. Be careful!',
+    'generate'           => 'Generate',
+    'token'              => 'Token|Tokens',
+    'current_tokens'     => 'Current Tokens',
+    'date'               => 'Date',
+    'comment'            => 'Comment',
+    'allowed_from'       => 'Allowed From',
+    'delete'             => 'Delete',
+    'logs'               => 'Logs',
+    'api_token_logs'     => 'API Token Access Logs',
+    'access_logs'        => 'Access Logs',
+    'action'             => 'Action',
+    'request_method'     => 'Request Method',
+    'request_path'       => 'Request Path',
+    'source_ip'          => 'Source IP',
+    'api_docs'           => 'API Documenation',
+
+];
