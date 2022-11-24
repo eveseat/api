fc81d08380996915d8d27dc36bea54b94c403311
Leon Jacobs <leonja511@gmail.com>
2022-11-24 23:45:43 +0200

    lang: add japanese support (#634)
    * New translations seat.php (Japanese)
    * New translations seat.php (Japanese)

---
+++ src/resources/lang/ja/seat.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of SeAT
+ *
+ * Copyright (C) 2015 to 2022 Leon Jacobs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+return [
+
+    'api_token_admin'    => '管理者APIトークン',
+    'new_token'          => '新しいトークン',
+    'key_comment'        => 'キーについてのメモ',
+    'allowed_ip_address' => '許可されているIPアドレス',
+    'ip_help'            => 'これは, 生成されたトークンの使用が許可されるソースIPアドレスです',
+    'ip_danger'          => '0.0.0.0を記述してしまうと, 悪意のある人を含むこのトークンを持っている全ての人がこのSeATのAPIを使用できるようになります. これは危険です. 使う際には細心の注意を払ってください.',
+    'generate'           => '生成',
+    'token'              => 'トークン | トークン一覧',
+    'current_tokens'     => '現在のトークン',
+    'date'               => '日時',
+    'comment'            => 'コメント',
+    'allowed_from'       => '許可された送信元',
+    'delete'             => '削除',
+    'logs'               => 'ログ',
+    'api_token_logs'     => 'APIトークンアクセスログ',
+    'access_logs'        => 'アクセスログ',
+    'action'             => 'アクション',
+    'request_method'     => 'リクエストメソッド',
+    'request_path'       => 'リクエストパス',
+    'source_ip'          => 'ソースIP',
+    'api_docs'           => 'APIドキュメント',
+
+];
