3e3105d8589665ab7764cf48e37f06a68807a708
Leon Jacobs <leonja511@gmail.com>
2025-02-21 08:57:17 +0200

    New translations seat.php (French)

--- src/resources/lang/fr/seat.php
+++ src/resources/lang/fr/seat.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of SeAT
  *
- * Copyright (C) 2015 to 2022 Leon Jacobs
+ * Copyright (C) 2015 to present Leon Jacobs
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -22,26 +22,25 @@
 
 return [
 
-    'api_token_admin'    => 'Administration des clés API',
-    'new_token'          => 'Nouvelle clé',
-    'key_comment'        => 'Commentaire de la clé',
+    'api_token_admin' => 'Administration des clés API',
+    'new_token' => 'Nouvelle clé',
+    'key_comment' => 'Commentaire de la clé',
     'allowed_ip_address' => 'Adresse IP autorisée',
-    'ip_help'            => 'Il s\'agit de l\'adresse IP source qui sera autorisée à utiliser la clé générée.',
-    'ip_danger'          => 'L\'utilisation de l\'adresse 0.0.0.0 permettra à N\'IMPORTE QUI possédant cette clé d\'utiliser l\'API SeAT. C\'est dangereux. Faites attention !',
-    'generate'           => 'Générer',
-    'token'              => 'Clé|Clés',
-    'current_tokens'     => 'Clés actuelles',
-    'date'               => 'Date',
-    'comment'            => 'Commentaire',
-    'allowed_from'       => 'Autorisé depuis',
-    'delete'             => 'Supprimer',
-    'logs'               => 'Journaux',
-    'api_token_logs'     => 'Journaux d\'accès à la clé API',
-    'access_logs'        => 'Journaux d\'accès',
-    'action'             => 'Action',
-    'request_method'     => 'Méthode de la requête',
-    'request_path'       => 'Chemin de la requête',
-    'source_ip'          => 'IP d\'origine',
-    'api_docs'           => 'Documentation de l\'API',
-
+    'ip_help' => 'This is the source IP address that will be allowed to use the generated token. Using 0.0.0.0 grants access to any IP.',
+    'ip_danger' => 'L\'utilisation de l\'adresse 0.0.0.0 permettra à N\'IMPORTE QUI possédant cette clé d\'utiliser l\'API SeAT. C\'est dangereux. Faites attention !',
+    'generate' => 'Générer',
+    'token' => 'Clé|Clés',
+    'current_tokens' => 'Clés actuelles',
+    'date' => 'Date',
+    'comment' => 'Commentaire',
+    'allowed_from' => 'Autorisé depuis',
+    'delete' => 'Supprimer',
+    'logs' => 'Journaux',
+    'api_token_logs' => 'Journaux d\'accès à la clé API',
+    'access_logs' => 'Journaux d\'accès',
+    'action' => 'Action',
+    'request_method' => 'Méthode de la requête',
+    'request_path' => 'Chemin de la requête',
+    'source_ip' => 'IP d\'origine',
+    'api_docs' => 'Documentation de l\'API',
 ];
