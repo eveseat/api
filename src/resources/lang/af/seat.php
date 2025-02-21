9ec1c62794f095754133ad5fa94ff4665836931a
Leon Jacobs <leonja511@gmail.com>
2025-02-21 08:57:18 +0200

    New translations seat.php (Afrikaans)

--- src/resources/lang/af/seat.php
+++ src/resources/lang/af/seat.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of SeAT
  *
- * Copyright (C) 2015 to 2022 Leon Jacobs
+ * Copyright (C) 2015 to present Leon Jacobs
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -22,26 +22,25 @@
 
 return [
 
-    'api_token_admin'    => 'API Token Admin',
-    'new_token'          => 'New Token',
-    'key_comment'        => 'Key Comment',
+    'api_token_admin' => 'API Token Admin',
+    'new_token' => 'New Token',
+    'key_comment' => 'Key Comment',
     'allowed_ip_address' => 'Allowed IP Address',
-    'ip_help'            => 'This is the source IP address that will be allowed to use the generated token.',
-    'ip_danger'          => 'Using 0.0.0.0 will allow ANYONE who has this token to use the SeAT API. This is dangerous. Be careful!',
-    'generate'           => 'Generate',
-    'token'              => 'Token|Tokens',
-    'current_tokens'     => 'Current Tokens',
-    'date'               => 'Date',
-    'comment'            => 'Comment',
-    'allowed_from'       => 'Allowed From',
-    'delete'             => 'Delete',
-    'logs'               => 'Logs',
-    'api_token_logs'     => 'API Token Access Logs',
-    'access_logs'        => 'Access Logs',
-    'action'             => 'Action',
-    'request_method'     => 'Request Method',
-    'request_path'       => 'Request Path',
-    'source_ip'          => 'Source IP',
-    'api_docs'           => 'API Documenation',
-
+    'ip_help' => 'This is the source IP address that will be allowed to use the generated token. Using 0.0.0.0 grants access to any IP.',
+    'ip_danger' => 'Using 0.0.0.0 will allow ANYONE who has this token to use the SeAT API. This is dangerous. Be careful!',
+    'generate' => 'Generate',
+    'token' => 'Token|Tokens',
+    'current_tokens' => 'Current Tokens',
+    'date' => 'Date',
+    'comment' => 'Comment',
+    'allowed_from' => 'Allowed From',
+    'delete' => 'Delete',
+    'logs' => 'Logs',
+    'api_token_logs' => 'API Token Access Logs',
+    'access_logs' => 'Access Logs',
+    'action' => 'Action',
+    'request_method' => 'Request Method',
+    'request_path' => 'Request Path',
+    'source_ip' => 'Source IP',
+    'api_docs' => 'API Documenation',
 ];
